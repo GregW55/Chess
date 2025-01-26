67af9f1be5a4e18b61b2c1b2738b8092eac40e68
GregW55 <163344650+GregW55@users.noreply.github.com>
2025-01-26 07:02:02 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,39 @@
+import pygame
+
+pygame.init()
+SCREEN_WIDTH = 803
+SCREEN_HEIGHT = 803
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+pygame.display.set_caption('Two-Player Pygame Chess!')
+font = pygame.font.Font('freesansbold.ttf', 20)
+
+piece_list = ['pawn', 'queen', 'king', 'knight', 'rook', 'bishop']
+# game variables and images
+
+# load in game piece images (queen, king, rook, bishop, knight, pawn) x 2
+black_queen = pygame.image.load('assets/black queen.png')
+black_queen = pygame.transform.scale(black_queen, (80, 80))
+black_king = pygame.image.load('assets/black king.png')
+black_king = pygame.transform.scale(black_king, (80, 80))
+black_rook = pygame.image.load('assets/black rook.png')
+black_rook = pygame.transform.scale(black_rook, (80, 80))
+black_bishop = pygame.image.load('assets/black bishop.png')
+black_bishop = pygame.transform.scale(black_bishop, (80, 80))
+black_knight = pygame.image.load('assets/black knight.png')
+black_knight = pygame.transform.scale(black_knight, (80, 80))
+black_pawn = pygame.image.load('assets/black pawn.png')
+black_pawn = pygame.transform.scale(black_pawn, (65, 65))
+white_queen = pygame.image.load('assets/white queen.png')
+white_queen = pygame.transform.scale(white_queen, (80, 80))
+white_king = pygame.image.load('assets/white king.png')
+white_king = pygame.transform.scale(white_king, (80, 80))
+white_rook = pygame.image.load('assets/white rook.png')
+white_rook = pygame.transform.scale(white_rook, (80, 80))
+white_bishop = pygame.image.load('assets/white bishop.png')
+white_bishop = pygame.transform.scale(white_bishop, (80, 80))
+white_knight = pygame.image.load('assets/white knight.png')
+white_knight = pygame.transform.scale(white_knight, (80, 80))
+white_pawn = pygame.image.load('assets/white pawn.png')
+white_pawn = pygame.transform.scale(white_pawn, (65, 65))
+white_images = [white_pawn, white_queen, white_king, white_knight, white_rook, white_bishop]
+black_images = [black_pawn, black_queen, black_king, black_knight, black_rook, black_bishop]
